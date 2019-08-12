aba1bb75187e333d7598a167e1a163414fa44812
Zakir Gowani <zakirgowani@gmail.com>
2019-08-12 12:18:13 -0500

    travis

--- src/stories/explorer.jsx
+++ src/stories/explorer.jsx
@@ -4,8 +4,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import DataExplorer from '../DataExplorer/.';
 
-import config from './explorerConfig';
-
 storiesOf('Data Explorer', module)
   .add('Data Explorer', () => {
     library.add(faAngleDown, faAngleUp);
